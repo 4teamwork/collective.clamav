79047da59154f55cd45d0bb6bb797311a521fd81
Daniel Jowett <d.jowett@4teamwork.ch>
2020-09-17 09:34:28 +0200

    Add logger

--- src/collective/clamav/__init__.py
+++ src/collective/clamav/__init__.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
-from zope.i18nmessageid import MessageFactory
 from Products.validation import validation
+from zope.i18nmessageid import MessageFactory
 
+import logging
 
 _ = MessageFactory('collective.clamav')
 
 
+# This import causes problems importing `_` if it comes before the line above
 from collective.clamav.validator import ClamavValidator  # noqa
-
 validation.register(ClamavValidator('isVirusFree'))
+
+logger = logging.getLogger('collective.clamav')
+logger.setLevel(logging.INFO)
+fh = logging.handlers.TimedRotatingFileHandler('collective.clamav.log', when='d', backupCount=7)
+fh.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
